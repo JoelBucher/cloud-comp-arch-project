0c64743c9020f3093845c40f563d9ee62e5c0f7a
Joel Bucher <mail@joelbucher.ch>
2024-05-14 11:31:02 +0200

    [untested] first version of scheduler

--- part_4/scheduler.py
+++ part_4/scheduler.py
@@ -12,26 +12,36 @@ client = docker.from_env()
 def get_cpu_usage():
     return psutil.cpu_percent(interval=1, percpu=True)
 
-job_settings = {
-    jobs.BLACKSCHOLES: {"image": "anakli/cca:parsec_blackscholes", "threads": 1},
-    jobs.CANNEAL: {"image": "anakli/cca:parsec_canneal", "threads": 4},
-    jobs.DEDUP: {"image": "anakli/cca:parsec_dedup", "threads": 4},
-    jobs.FERRET: {"image": "anakli/cca:parsec_ferret", "threads": 8},
-    jobs.FREQMINE: {"image": "anakli/cca:parsec_freqmine", "threads": 8},
-    jobs.VIPS: {"image": "anakli/cca:parsec_vips", "threads": 4},
-    jobs.RADIX: {"image": "anakli/cca:splash2x_radix", "threads": 2}
+configs = {
+    jobs.BLACKSCHOLES: {"name": "blackscholes", "image": "anakli/cca:parsec_blackscholes", "threads": 1},
+    jobs.CANNEAL: {"name": "canneal", "image": "anakli/cca:parsec_canneal", "threads": 4},
+    jobs.DEDUP: {"name": "dedup", "image": "anakli/cca:parsec_dedup", "threads": 4},
+    jobs.FERRET: {"name": "ferret", "image": "anakli/cca:parsec_ferret", "threads": 8},
+    jobs.FREQMINE: {"name": "freqmine", "image": "anakli/cca:parsec_freqmine", "threads": 8},
+    jobs.VIPS: {"name": "vips", "image": "anakli/cca:parsec_vips", "threads": 4},
+    jobs.RADIX: {"name": "radix", "image": "anakli/cca:splash2x_radix", "threads": 2}
+}
+
+def create_container(job):
+    return {
+    'image': job["image"],
+    'command': "./run -a run -S parsec -p %s -i native -n %s" % (job['name'],job['threads']),
+    'cpuset_cpus': '0',
+    'detach': True,
+    'remove': True,
+    'name': 'parsec'
 }
 
 def main():
     logger = scheduler_logger.SchedulerLogger
 
-    for j in jobs:
-        logger.job_start(j)
-        logger.job_end(j)
-        container = client.containers.run(j["image"], f"./run -a run -S parsec -p {j.value} -i native -n {num_threads}", cpuset_cpus=",".join(map(str, [0])), detach=True)
-    print("work in progress")
-    
-    print(get_cpu_usage())
+    for c in configs:
+        name = c["name"]
+        logger.job_start(name)
+        container = client.containers.run(**create_container(c))
+        container.wait()
+        logger.job_end(name)
+        get_cpu_usage()
 
 if __name__ == "__main__":
     main()
