8aa0cb075581fcdd1097d8ea714c53b652361c92
Joel Bucher <mail@joelbucher.ch>
2024-05-14 15:44:47 +0200

    basic sequential scheduler

--- part_4/scheduler.py
+++ part_4/scheduler.py
@@ -1,47 +1,58 @@
 import psutil
 import scheduler_logger 
-from enum import Enum
 import docker
 
-logger = scheduler_logger.SchedulerLogger
-jobs = scheduler_logger.Job
-
 client = docker.from_env()
+Job = scheduler_logger.Job
 
 # returns an array of cpu usage in percent, where the index i is the core i
 def get_cpu_usage():
     return psutil.cpu_percent(interval=1, percpu=True)
 
 configs = {
-    jobs.BLACKSCHOLES: {"name": "blackscholes", "image": "anakli/cca:parsec_blackscholes", "threads": 1},
-    jobs.CANNEAL: {"name": "canneal", "image": "anakli/cca:parsec_canneal", "threads": 4},
-    jobs.DEDUP: {"name": "dedup", "image": "anakli/cca:parsec_dedup", "threads": 4},
-    jobs.FERRET: {"name": "ferret", "image": "anakli/cca:parsec_ferret", "threads": 8},
-    jobs.FREQMINE: {"name": "freqmine", "image": "anakli/cca:parsec_freqmine", "threads": 8},
-    jobs.VIPS: {"name": "vips", "image": "anakli/cca:parsec_vips", "threads": 4},
-    jobs.RADIX: {"name": "radix", "image": "anakli/cca:splash2x_radix", "threads": 2}
-}
-
-def create_container(job):
-    return {
-    'image': job["image"],
-    'command': "./run -a run -S parsec -p %s -i native -n %s" % (job['name'],job['threads']),
-    'cpuset_cpus': '0',
-    'detach': True,
-    'remove': True,
-    'name': 'parsec'
+    Job.BLACKSCHOLES: {"image": "anakli/cca:parsec_blackscholes", "threads": 1},
+    Job.CANNEAL: {"image": "anakli/cca:parsec_canneal", "threads": 4},
+    Job.DEDUP: {"image": "anakli/cca:parsec_dedup", "threads": 4},
+    Job.FERRET: {"image": "anakli/cca:parsec_ferret", "threads": 8},
+    Job.FREQMINE: {"image": "anakli/cca:parsec_freqmine", "threads": 8},
+    Job.VIPS: {"image": "anakli/cca:parsec_vips", "threads": 4},
+    Job.RADIX: {"image": "anakli/cca:splash2x_radix", "threads": 2}
 }
 
 def main():
-    logger = scheduler_logger.SchedulerLogger
+    logger = scheduler_logger.SchedulerLogger()
 
-    for c in configs:
-        name = c["name"]
-        logger.job_start(name)
-        container = client.containers.run(**create_container(c))
+    for j in Job:
+        # We dont want to execute the scheduler task
+        if j == Job.SCHEDULER or j== Job.MEMCACHED:
+            continue
+
+        settings = configs[j]
+        image = settings["image"]
+        threads = settings["threads"]
+
+        logger.job_start(j,[],0)
+        print(j)
+        print(j.value)
+        container = client.containers.run(
+            image= image,
+            command= ["./run", "-a", "run", "-S", "parsec", "-p", j.value, "-i", "native", "-n", str(threads)],
+            cpuset_cpus= '2',
+            detach= True,
+            remove= False,
+            name= j.value
+        )
+        
         container.wait()
-        logger.job_end(name)
-        get_cpu_usage()
+
+        # while True:
+        #     if(container.status != "running"):
+        #         break
+        #     container.reload()
+
+        logger.job_end(j)
+        print(container.logs())
+        
 
 if __name__ == "__main__":
     main()
